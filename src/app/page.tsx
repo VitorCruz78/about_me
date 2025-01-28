ad231bbb010fcd1e8ab41c5876b308da7df0ffab
Vitor Luiz da Cruz <122692132+VitorCruz78@users.noreply.github.com>
2025-01-28 07:53:13 -0300

    fix: min-w-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   const [showContact, setShowContact] = useState<boolean>(false)
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start lg:justify-center relative">
+    <div className="min-w-screen min-h-screen flex flex-col items-center justify-start lg:justify-center relative">
       <aside
         className={`
           transition-all ease-in-out duration-300 overflow-hidden ${!showContact ? 'h-0' : 'h-28'}
