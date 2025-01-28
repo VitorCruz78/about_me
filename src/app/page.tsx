8a6ad12f79d825e68f469accb468be9e498cdf3e
VitorCruz78 <vitorluiz15012006@gmail.com>
2025-01-27 21:28:16 -0300

    feat: menu mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import Image from "next/image";
-import Me from "../assets/image_profile.png"
+import Me from "../assets/image_profile.png";
 import Link from "next/link";
 import { useState } from "react";
-import { List, X } from "@phosphor-icons/react";
+import { ChatCenteredText, X } from "@phosphor-icons/react";
 
 interface IProjects {
   title: string
@@ -22,27 +22,36 @@ export default function Home() {
     { title: "MiniBlog", description: "Um miniblog feito em ReactJS e firebase", link: "https://github.com/VitorCruz78/MiniBlog", tag: ["F", "B"] },
     { title: "Hello World", description: "Esse dispensa comentários. (Foi só para completar 6 caixinhas e deixar o layout bacana)", link: "https://github.com/VitorCruz78/Ola-Mundo", tag: [] },
   ]
-  const [showMenu, setShowMenu] = useState<boolean>(false)
+  const [showContact, setShowContact] = useState<boolean>(false)
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start lg:justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-start lg:justify-center relative">
       <aside
         className={`
-          overflow-hidden transition-all ease-in-out duration-100 z-50 bg-defaultColor
-          fixed right-0 top-0 ${showMenu ? 'w-[50vw] xl:w-[32rem] border-l border-r-defaultBorderColor px-6 py-12' : 'w-0'} h-screen shadow-xl
-          flex flex-col items-start justify-start gap-6
+          transition-all ease-in-out duration-300 overflow-hidden ${!showContact ? 'h-0' : 'h-28'}
+          fixed inset-0 w-full bg-defaultColor shadow-xl z-50 flex flex-col items-center justify-center sm:hidden
         `}
       >
-        <X size={42} className="text-defaultTextColor cursor-pointer hover:opacity-50 transition-all ease-in-out duration-300" onClick={() => setShowMenu(false)} />
-        <Link href={'https://www.linkedin.com/in/vitor-luiz-b65b87233/'} target="_blank" className="text-xl text-[#0E76A8] font-medium hover:opacity-90">Linkedin</Link>
-        <Link
-          href={'https://www.instagram.com/vitor.tnh/'}
-          target="_blank"
-          className="text-xl font-medium hover:opacity-90 text-transparent bg-clip-text bg-gradient-to-r from-[#DD2A7B] to-[#8134AF]">
-          Instagram
-        </Link>
-        <Link href={'https://wa.me/47992394425'} target="_blank" className="text-xl text-[#25D366] font-medium hover:opacity-90">Whatsapp</Link>
+        <div className="w-full h-full flex flex-col items-end justify-evenly">
+          <X 
+           size={24}
+           className="text-defaultTextColor mr-4"
+           onClick={() => setShowContact(false)}
+          />
+          <div className="w-full flex flex-row flex-wrap items-center justify-center gap-6">
+            <Link href={'https://www.linkedin.com/in/vitor-luiz-b65b87233/'} target="_blank" className="text-base text-[#0E76A8] font-medium hover:opacity-90">Linkedin</Link>
+            <Link
+              href={'https://www.instagram.com/vitor.tnh/'}
+              target="_blank"
+              className="text-base font-medium hover:opacity-90 text-transparent bg-clip-text bg-gradient-to-r from-[#DD2A7B] to-[#8134AF]">
+              Instagram
+            </Link>
+            <Link href={'https://wa.me/47992394425'} target="_blank" className="text-base text-[#25D366] font-medium hover:opacity-90">Whatsapp</Link>
+            <Link href={'https://github.com/VitorCruz78'} target="_blank" className="text-base text-[#F0F6FC] font-medium hover:opacity-90">GitHub</Link>
+          </div>
+        </div>
       </aside>
+
       <div className="w-screen h-auto p-6">
         <main className="flex flex-col items-center justify-center gap-6">
           <div className="w-full md:max-w-[32rem] lg:max-w-[48rem] flex flex-row items-start justify-center relative">
@@ -54,9 +63,6 @@ export default function Home() {
               quality={100}
               className="rounded-full"
             />
-            <p className="absolute top-0 right-0 cursor-pointer hover:opacity-50 transition-all ease-in-out duration-300">
-              {!showMenu && <List size={42} className="text-defaultTextColor" onClick={() => setShowMenu(true)} />}
-            </p>
           </div>
           <h1 className="text-sm text-defaultTextColor w-full md:w-[32rem] lg:w-[38rem] text-center leading-[1.6rem] tracking-wide">
             Oi, meu nome é Vitor Luiz, tenho 19 anos e trabalho como desenvolvedor há 2 anos. Curto tanto Front-end quanto Back-end, com um foco especial em JavaScript. Tenho experiência com HTML5, CSS3, Tailwind CSS, ReactJS, React Native, NextJS, NodeJS, ExpressJS, Prisma ORM e TypeScript.
@@ -68,7 +74,7 @@ export default function Home() {
                 <div
                   key={projects.title}
                   className="
-                    w-80 md:w-60 h-60 border border-defaultBorderColor p-2 rounded-md flex flex-col items-start justify-start gap-6 relative
+                    w-80 md:w-60 h-60 border border-defaultBorderColor/50 p-2 rounded-md flex flex-col items-start justify-start gap-6 relative
                     transition-all ease-in-out duration-300 hover:opacity-90
                   "
                 >
@@ -79,14 +85,14 @@ export default function Home() {
                         <div className="flex flex-row items-center gap-2">
                           {
                             projects.tag.map(tags => (
-                              <div key={tags} className={`w-10 p-2 rounded-md shadow-xl border border-defaultBorderColor ${tags === "F" ? "bg-defaultBlueColor" : "bg-defaultColor"}`}>
+                              <div key={tags} className={`w-10 p-2 rounded-md shadow-xl border border-defaultBorderColor/50 ${tags === "F" ? "bg-defaultBlueColor" : "bg-defaultColor"}`}>
                                 <p className="text-center text-xs text-defaultTextColor font-bold">{tags}</p>
                               </div>
                             ))
                           }
                         </div>
                         :
-                        <div className={`w-20 p-2 rounded-md shadow-xl border border-defaultBorderColor ${projects.tag === "Front-end" ? "bg-defaultBlueColor" : "bg-defaultColor"}`}>
+                        <div className={`w-20 p-2 rounded-md shadow-xl border border-defaultBorderColor/50 ${projects.tag === "Front-end" ? "bg-defaultBlueColor" : "bg-defaultColor"}`}>
                           <p className="text-center text-xs text-defaultTextColor font-bold">{projects.tag}</p>
                         </div>
                     }
@@ -98,6 +104,27 @@ export default function Home() {
             }
           </div>
         </main>
+      </div>
+      <div className="absolute left-12 top-12">
+        <div className="hidden sm:flex sm:flex-col lg:flex lg:flex-row items-center gap-6">
+          <Link href={'https://www.linkedin.com/in/vitor-luiz-b65b87233/'} target="_blank" className="text-xl text-[#0E76A8] font-medium hover:opacity-90">Linkedin</Link>
+          <Link
+            href={'https://www.instagram.com/vitor.tnh/'}
+            target="_blank"
+            className="text-xl font-medium hover:opacity-90 text-transparent bg-clip-text bg-gradient-to-r from-[#DD2A7B] to-[#8134AF]">
+            Instagram
+          </Link>
+          <Link href={'https://wa.me/47992394425'} target="_blank" className="text-xl text-[#25D366] font-medium hover:opacity-90">Whatsapp</Link>
+          <Link href={'https://github.com/VitorCruz78'} target="_blank" className="text-xl text-[#F0F6FC] font-medium hover:opacity-90">GitHub</Link>
+        </div>
+        <div className="block sm:hidden">
+          <ChatCenteredText
+            weight="fill"
+            size={32}
+            className="text-defaultTextColor"
+            onClick={() => setShowContact(true)}
+          />
+        </div>
       </div>
     </div>
   );
